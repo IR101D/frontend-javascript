91ed6d2ab9d0712ae6e2813b505200e1c567a8cb
IR101D <somefkingadress@gmail.com>
2025-09-15 19:45:19 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -40,11 +40,9 @@ interface printTeacherFunction {
 }
 
 //function printTeacher that displays first letter of firstname and full lastname
-const printTeacher : printTeacherFunction = (FirstName : string , lastName : string) : string =>
- {
-  const firstInitial = FirstName.charAt(0).toUpperCase() ;
-  return '$firstInitial, $lastName';
- };
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
 
  //interface for the class
  interface StudentClassInterface {
